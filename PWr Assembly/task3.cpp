2b55f0eefcdb91bc1fe184d563764614175ae327
Davs <davialbuq.comp@gmail.com>
2021-05-21 17:10:20 +0200

    Task1 needs to be deggubed and improved

---
+++ PWr Assembly/task3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdlib>
+
+int main(){
+	int n;
+	int j;	
+	cout << "\nInsert a number smaller than 100: ";
+	cin >> n;
+	cout << "\nYou will be asked for " << n << "values.";
+	
+	for(int i = 0; i < n; i++){
+		j = i + 1;	
+		cout << n << "th value: ";
+		
+	}
+	
+	
+	
+	
+	return 0;
+}
